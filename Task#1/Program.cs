d262e20abfbd8e5364159111674acd2e07b381dc
Borodin <Fallenlord48@gmail.com>
2022-11-10 18:02:25 +0300

    Решение задачи о выборке из массива строк в новый массив строк

--- Task#1/Program.cs
+++ Task#1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+string[] array = new string[] {"Russia", "Denmark", "Kazan", "-67", "-677", "UFA"};
+string[] arrayNew = new string[array.Length];
+void ArrayNewWithIF(string[] array, string[] arrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        arrayNew[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayNewWithIF(array, arrayNew);
+PrintArray(arrayNew);
